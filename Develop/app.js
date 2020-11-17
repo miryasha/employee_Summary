3d20471a86b3a890b969ae2c346687919b3b67a1
yasha6081 <yasha6081@gmail.con>
2020-11-17 00:05:25 -0600

    adding functions to app

--- Develop/app.js
+++ Develop/app.js
@@ -7,9 +7,13 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
+ function addManager(){}
+ function addEmployee(){}
+ function addEngineer(){}
+ function addIntern(){}
+ function employeeSelector(){}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
